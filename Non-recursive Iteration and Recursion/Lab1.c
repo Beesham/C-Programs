89b98639642f88c25df05e1883162126bddd04fa
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-06-28 19:46:01 -0400

    Add Lab1 and Lab2 stubs and started on Lab1

---
+++ Non-recursive Iteration and Recursion/Lab1.c
@@ -0,0 +1,70 @@
+/*
+	Author: Beesham Sarendranauth, 104854956
+	Date: 2017/07/03
+	Description: performs non-recursive operations such as factorials
+				 and fibonnacci sequence.
+				 The operations are selected from a menu presented to
+				 the user.
+*/
+#include <stdio.h>
+
+//Function prototypes
+void summation();
+void factorial();
+void fibonacci();
+void gcd();
+void power();
+
+
+int main(void){
+
+	puts("\tMenu");
+	puts("\t----");
+
+
+}
+
+/*
+	Objective:
+	Input:
+	Output:
+*/
+void summation(){
+
+}
+
+/*
+	Objective:
+	Input:
+	Output:
+*/
+void factorial(){
+	
+}
+
+/*
+	Objective:
+	Input:
+	Output:
+*/
+void fibonacci(){
+	
+}
+
+/*
+	Objective:
+	Input:
+	Output:
+*/
+void gcd(){
+	
+}
+
+/*
+	Objective:
+	Input:
+	Output:
+*/
+void power(){
+
+}
---
+++ Template.c
@@ -0,0 +1,10 @@
+/*
+	Author: Beesham Sarendranauth, 104854956
+	Date:
+	Description:
+*/
+#include <stdio.h>
+
+int main(void){
+
+}
