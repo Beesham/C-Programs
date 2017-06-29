a9ce0102b2d8372cc4d79fdc32e042dcfce13fa5
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-06-28 20:16:48 -0400

    Fix: actually fix indentations in Lab1

--- Non-recursive Iteration and Recursion/Lab1.c
+++ Non-recursive Iteration and Recursion/Lab1.c
@@ -1,10 +1,10 @@
 /*
-    Author: Beesham Sarendranauth, 104854956
-    Date: 2017/07/03
-    Description: performs non-recursive operations such as factorials
-		 and fibonnacci sequence.
-		 The operations are selected from a menu presented to
-		 the user.
+  Author: Beesham Sarendranauth, 104854956
+  Date: 2017/07/03
+  Description: performs non-recursive operations such as factorials
+   and fibonnacci sequence.
+   The operations are selected from a menu presented to
+   the user.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,91 +18,91 @@ void power();
 
 
 int main(void){
-	
-	int selection;
+  
+  int selection;
     
-	while(selection != EOF){ 
+  while(selection != EOF){ 
     
-        puts("*************");
-        puts("*   Menu    *");
-        puts("*************");
-        puts("1. Summation");
-        puts("2. Factorial");
-        puts("3. Fibonacci");
-        puts("4. GCD");
-        puts("5. Power");
+    puts("*************");
+    puts("*   Menu    *");
+    puts("*************");
+    puts("1. Summation");
+    puts("2. Factorial");
+    puts("3. Fibonacci");
+    puts("4. GCD");
+    puts("5. Power");
 
-        puts("");
-        puts("Please select the number index of your operation e.g 1. \nEnter -1 to quit");
-        printf("%s","Input:");
-        scanf("%d", &selection);
+    puts("");
+    puts("Please select the number index of your operation e.g 1. \nEnter -1 to quit");
+    printf("%s","Input:");
+    scanf("%d", &selection);
 
-	
-        switch(selection){
-            case 1:
-            break;
-            
-            case 2:
-            break;
-            
-            case 3:
-            break;
-            
-            case 4:
-            break;
-            
-            case 5:
-            break;
-                    
-            default:
-                puts("ERROR: selection must be from the menu shown");
-        }
+  
+    switch(selection){
+      case 1:
+      break;
+      
+      case 2:
+      break;
+      
+      case 3:
+      break;
+      
+      case 4:
+      break;
+      
+      case 5:
+      break;
+                
+      default:
+          puts("ERROR: selection must be from the menu shown");
+    }
         
-        getchar();  //Clears the input buffer
-	}
-	
+    getchar();  //Clears the input buffer
+  }
+  
 }
 
 /*
-     Objective:
-    Input:
-    Output:
+  Objective:
+  Input:
+  Output:
 */
 void summation(){
 
 }
 
 /*
-    Objective:
-    Input:
-    Output:
+  Objective:
+  Input:
+  Output:
 */
 void factorial(){
-	
+  
 }
 
 /*
-    Objective:
-    Input:
-    Output:
+  Objective:
+  Input:
+  Output:
 */
 void fibonacci(){
-	
+  
 }
 
 /*
-    Objective:
-    Input:
-    Output:
+  Objective:
+  Input:
+  Output:
 */
 void gcd(){
-	
+  
 }
 
 /*
-    Objective:
-    Input:
-    Output:
+  Objective:
+  Input:
+  Output:
 */
 void power(){
 
