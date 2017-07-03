642b951cf4b3b8a0eb7aae1a711687ef723f756f
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-07-03 19:04:21 -0400

    Add assign_1.c with function stubs and docs

---
+++ Recursion, Searching, Sorting, and Pointers/assign_1.c
@@ -0,0 +1,34 @@
+/*
+	Author: Beesham Sarendranauth, 104854956
+	Date: July 12, 2017
+	Description: This program demonstrated the sorting of a matrix of integers
+*/
+#include <stdio.h>
+#define COL 20
+#define ROW 20
+
+//function prototypes
+int sortMatrix(int A[][], unsigned int rowsize, unsigned int colsize);
+int searchMatrix(int A[][], int V, int *P, unsigned int rowsize, unsigned int colsize);
+
+int main(void){
+
+}
+
+/*
+    sortMatrix:
+    input:
+    output:
+*/
+int sortMatrix(int A[][COL], unsigned int rowsize, unsigned int colsize){
+
+}
+
+/*
+    searchMatrix:
+    input:
+    output:
+*/
+int searchMatrix(int A[][COL], int V, int *P, unsigned int rowsize, unsigned int colsize){
+  
+}
