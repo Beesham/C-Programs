8dc5a9cb03ab6fdff210e0e70c80316c7434624d
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-07-04 19:26:08 -0400

    Start impl of sortMatrix in assigb1.c

--- Recursion, Searching, Sorting, and Pointers/assign_1.c
+++ Recursion, Searching, Sorting, and Pointers/assign_1.c
@@ -13,7 +13,24 @@ int sortMatrix (unsigned int rowsize, unsigned int colsize, int A[rowsize][colsi
 int searchMatrix (int V, int *P, unsigned int rowsize, unsigned int colsize, int A[rowsize][colsize]);
 
 int main(void){
-    int array[][6]={{4,8,6,9,4}, {8,6,2,5,4,4}};
+    int array[2][6]={{4,8,6,9,4}, {8,6,2,5,4,7}};
+    
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 6; j++){
+            printf("%2i", array[i][j]);
+        } 
+        puts("");
+    }
+    
+    int sortedArr = sortMatrix(2, 6, array);
+    puts("");
+    
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 6; j++){
+            printf("%2i", array[i][j]);
+        } 
+        puts("");
+    }
 }
 
 /*
@@ -23,7 +40,30 @@ int main(void){
     output: returns 1 (DONE) when sort finishes
 */
 int sortMatrix (unsigned int rowsize, unsigned int colsize, int A[][colsize]){
+    int swap = 0;
+    int sizeOfArray = rowsize * colsize;
     
+    for(unsigned int pass = 0; pass < sizeOfArray - pass - 1 && !swap; pass++){
+        //traverses the matrix of integers
+        for(unsigned int i = 0; i < rowsize; i++){
+            for(unsigned int j = 0; j < colsize; j++){
+                int hold = A[i][j];
+                //checks if current num is less than next, if so then swap
+                if(A[i][j] > A[i][j+1]){
+                    
+                    printf("before swap: %2i\n", A[i][j]);
+                    
+                    hold = A[i][j];
+                    A[i][j] = A[i][j+1];
+                    A[i][j+1] = hold;
+                    swap = 0;
+                    
+                    printf("after swap: %2i\n", A[i][j]);
+
+                }
+            }
+        }
+    }
     
     
     return 0;
