948de089effb1147d24698894b5893c5d792de95
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-07-04 18:43:46 -0400

    Fix: compilation err (array type has incomplete ele... int[]) in assign1.c

--- Recursion, Searching, Sorting, and Pointers/assign_1.c
+++ Recursion, Searching, Sorting, and Pointers/assign_1.c
@@ -6,22 +6,27 @@
 #include <stdio.h>
 #define COL 20
 #define ROW 20
+#define DONE 1
 
 //function prototypes
-int sortMatrix(int A[][], unsigned int rowsize, unsigned int colsize);
-int searchMatrix(int A[][], int V, int *P, unsigned int rowsize, unsigned int colsize);
+int sortMatrix (unsigned int rowsize, unsigned int colsize, int A[rowsize][colsize]);
+int searchMatrix (int V, int *P, unsigned int rowsize, unsigned int colsize, int A[rowsize][colsize]);
 
 int main(void){
-
+    int array[][6]={{4,8,6,9,4}, {8,6,2,5,4,4}};
 }
 
 /*
-    sortMatrix:
-    input:
-    output:
+    sortMatrix: sorts a matrix of size NxM in descending order i.e. A[0][0] would have the largest value
+    and A[N][M] would have the smallest value.
+    input: 2 dimensional array A[][], the row (rowsize) and column (colsize) size
+    output: returns 1 (DONE) when sort finishes
 */
-int sortMatrix(int A[][COL], unsigned int rowsize, unsigned int colsize){
-
+int sortMatrix (unsigned int rowsize, unsigned int colsize, int A[][colsize]){
+    
+    
+    
+    return 0;
 }
 
 /*
@@ -29,6 +34,7 @@ int sortMatrix(int A[][COL], unsigned int rowsize, unsigned int colsize){
     input:
     output:
 */
-int searchMatrix(int A[][COL], int V, int *P, unsigned int rowsize, unsigned int colsize){
-  
+int searchMatrix (int V, int *P, unsigned int rowsize, unsigned int colsize, int A[][colsize]){
+    
+    return 0;
 }
