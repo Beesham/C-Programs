2e03932325653bf738068f03b8ae19d905d45d18
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-07-06 20:21:17 -0400

    Start impl of searchMatrix using pointer offset notation

--- Recursion, Searching, Sorting, and Pointers/assign_1.c
+++ Recursion, Searching, Sorting, and Pointers/assign_1.c
@@ -7,8 +7,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define COL 20
-#define ROW 20
+#define COL 3
+#define ROW 3
+#define SIZE COL * ROW
 #define DONE 1
 
 //function prototypes
@@ -19,27 +20,33 @@ int main(void){
     
     srand(time(NULL));
     
-    int array[ROW][COL] = {0};    //init array to 0
+    int array[ROW][COL] = {{2,3,4},{5,6,7},{8,9, 10}};    //init array to 0
+    int searchedNumPosition[2] = {0, SIZE - 1};
     
     //gens a random num for each element in the array
     for (int i = 0; i < ROW; i++){
         for (int j = 0; j < COL; j++){
-            array[i][j] = rand() % 101; //assigns a random positive int from 0 - 100 to a position in the array
+            //array[i][j] = rand() % 101; //assigns a random positive int from 0 - 100 to a position in the array 
+                                        //(0 + (rand() % (100 + 1 - 0)) is the formula used to get 101
         }
     }
     
-    for(int i = 0; i < ROW; i++){
-        for(int j = 0; j < COL; j++){
+    for (int i = 0; i < ROW; i++){
+        for (int j = 0; j < COL; j++){
             if (j % 10 == 0) puts("");
             printf(" %3d ", array[i][j]);
         } 
     }
     
-    int sortedArr = sortMatrix(ROW, COL, array);
+    //int sortedArr = sortMatrix(ROW, COL, array);
     puts("");
     
-    for(int i = 0; i < ROW; i++){
-        for(int j = 0; j < COL; j++){
+    int isFound = searchMatrix(9, searchedNumPosition, ROW, COL, array);
+    printf("\nisFOund: %d\n", isFound);
+
+    
+    for (int i = 0; i < ROW; i++){
+        for (int j = 0; j < COL; j++){
             if (j % 10 == 0) puts("");
             printf(" %3d ", array[i][j]);
         } 
@@ -48,7 +55,7 @@ int main(void){
 
 /*
     sortMatrix: sorts a matrix of size NxM in descending order i.e. A[0][0] would have the largest value
-    and A[N][M] would have the smallest value.
+    and A[N][M] would have the smallest value
     input: 2 dimensional array A[][], the row (rowsize) and column (colsize) size
     output: returns 1 (DONE) when sort finishes
 */
@@ -77,11 +84,78 @@ int sortMatrix (unsigned int rowsize, unsigned int colsize, int A[][colsize]){
 }
 
 /*
-    searchMatrix:
-    input:
-    output:
+    searchMatrix: searches a sorted matrix for a given value using recursive binary search.
+                  it also computes the position of the value in the matrix
+    input: the value to be searched (V)
+           the size of the matrix (rowsize, colsize)
+           the array (A[][])
+           a pointer to the potential position of the value
+    output: 1 if the value is found, otherwise -1
 */
 int searchMatrix (int V, int *P, unsigned int rowsize, unsigned int colsize, int A[][colsize]){
+    int foundNum = -1;
+    int minOffset = *P;
+    int maxOffset = *(P + 1);
+    int middleOffset = (maxOffset + minOffset) / 2;
     
-    return 0;
+    int *ptr = &A[0][0];
+    
+    printf("\nminOff: %d maxOff: %d midOff: %d\n", minOffset, maxOffset, middleOffset);
+        
+    int minValue = *(ptr + minOffset);
+    int maxValue = *(ptr + maxOffset);
+    int middleValue = *(ptr + middleOffset);
+
+    printf("\nA min: %d, max: %d, mid: %d\n", minValue, maxValue, middleValue);
+
+    //base case
+    if (V == middleValue) return 1;
+    if (V > middleValue){
+        *P = middleOffset;
+        searchMatrix(V, P, rowsize, colsize, A);
+    }
+    else if (V < middleValue){
+        *P = minOffset;
+        *(P + 1) = middleOffset;
+        searchMatrix(V, P, rowsize, colsize, A);
+    }
+    /*
+    else if (V > A[rowsize/2][colsize/2]){
+        searchMatrix(V, P, rowsize/2, colsize/2, A);
+    }
+    */
+        
+    
+    return foundNum;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
