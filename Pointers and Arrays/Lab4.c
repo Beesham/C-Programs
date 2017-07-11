359ae2d9ffc5729f2b7ae58688482287d85135ae
beesham <sarendranauth.beesham@gmail.com>
2017-07-11 18:32:15 -0400

    Add Lab4.c

---
+++ Pointers and Arrays/Lab4.c
@@ -0,0 +1,14 @@
+/*
+    Author: Beesham Sarendranauth
+	Date: 2017/07/18
+	Description: performs tasks using pointer operations, such as finding 
+				largest number, or swaping   
+*/
+
+#include <studio.h>
+#include <stdlib.h>
+
+int main (void){
+
+
+}
