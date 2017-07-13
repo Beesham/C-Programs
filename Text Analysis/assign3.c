0e949db0d8c11258c4d1e282cc75b42063cdff6d
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-07-13 18:13:28 -0400

    Add assign3 stub

---
+++ Text Analysis/assign3.c
@@ -0,0 +1,51 @@
+/*
+	Author: Beesham Sarendranauth, 104854956
+	Date: 2017/07/19
+	Description: This program analyses text. It determines how many times words are used,
+    letter repetition, and word length repetition
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINES 10
+#define MAX_LINE_LENGTH 80
+
+//prototypes
+void letterAnalysis(char *buffer, int numOfLines);
+int wordLengthAnalysis(char *buffer, int numOfLines, int lenght);
+void wordAnalysis(char *buffer, int numOfLines);
+void getText(char *buffer);
+
+int main(void){
+
+}
+
+/*
+
+*/
+void letterAnalysis(char *buffer, int numOfLines){
+    
+}
+
+/*
+
+*/
+int wordLengthAnalysis(char *buffer, int numOfLines, int lenght){
+    
+    return 0;
+}
+
+/*
+
+*/
+void wordAnalysis(char *buffer, int numOfLines){
+    
+}
+
+/*
+
+*/
+void getText(char *buffer){
+    
+}
