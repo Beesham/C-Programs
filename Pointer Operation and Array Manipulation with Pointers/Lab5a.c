47c7c561f36e84a1fa3b28870fe7b6fc5939e215
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-07-15 18:19:25 -0400

    Docs:Add docs to Lab5

--- Pointer Operation and Array Manipulation with Pointers/Lab5a.c
+++ Pointer Operation and Array Manipulation with Pointers/Lab5a.c
@@ -10,15 +10,15 @@ int main(void){
     int *aPtr;
     aPtr = &a;
     
-    printf("%p", &a);
-    printf("%p", aPtr);
-    printf("%p", &aPtr);
-    printf("%d", a);
-    printf("%d", *aPtr);
-    printf("%p", *&aPtr);
-    printf("%p", &*aPtr);
-    printf("%d", *&a);
-    printf("%d", &*a);
+    printf("%p", &a);       //prints the address of var 'a'
+    printf("%p", aPtr);     //prints the address of var 'a' through a pointer
+    printf("%p", &aPtr);    //prints the address of 'aPtr' itself
+    printf("%d", a);        //prints the value of var 'a'
+    printf("%d", *aPtr);    //prints the value of var 'a' through pointer dereferencing
+    printf("%p", *&aPtr);   //prints the address of 'aPtr' itself because * and & are compliments of each other
+    printf("%p", &*aPtr);   //prints the address of 'aPtr' itself because * and & are compliments of each other
+    printf("%d", *&a);      //prints the value of var 'a' through address dereferencing 
+    //printf("%d", &*a);      //error, var 'a' is not a pointer. &*a is interpreted as ampersand multiplied by var 'a'
     
     return 0;
 }
