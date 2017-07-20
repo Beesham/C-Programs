37c9b3a2d333cbe0d665e8c849e837f623fbd67b
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-07-20 18:28:39 -0400

    Read input file until EOF is encoutered

--- Strings and Structures/assign4.c
+++ Strings and Structures/assign4.c
@@ -8,30 +8,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LINE_LENGTH 256
+#define MAX_COURSES 10
+#define MAX_COURSE_NAME_LENGTH 50
+#define MAX_COURSE_CODE_LENGTH 12
+#define MAX_TERM_LENGTH 6
+
+
+
+typedef struct {
+    int courseID;
+    char courseName[MAX_COURSE_NAME_LENGTH];
+    char courseCode[MAX_COURSE_CODE_LENGTH];
+    char term[MAX_TERM_LENGTH];
+} CourseInfo;
+
+
 //prototypes
-void loadCourseInfo();
+void loadCourseInfo(CourseInfo *courses);
 void addCourseInfo();
 void searchCourseInfo();
 void displayCourseInfo();
 
-typedef struct {
-    int courseID;
-    char courseName[50];
-    char courseCode[12];
-    char term[6];
-} CourseInfo;
-
-
 int main(void){
     
+    CourseInfo courses[MAX_COURSES];
     
-    
+    loadCourseInfo(courses);
 }
 
 /*
-
+    loadCourseInfo: reads data relating to courses from a csv and stores it in an array of CourseInfo
+    input: the array to store the data (courses)
 */
-void loadCourseInfo(){
+void loadCourseInfo(CourseInfo *courses){
+        
+    char courseRecordBuffer[MAX_LINE_LENGTH];
+    
+    while(fgets(courseRecordBuffer, MAX_LINE_LENGTH, stdin) != NULL){
+        printf("%s", courseRecordBuffer);
+    }
     
 }
 
