d18acfa591a07cf9f7930a6c3832dbafcc265ab1
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-07-20 17:42:35 -0400

    Add method stubs in assign4

--- Strings and Structures/assign4.c
+++ Strings and Structures/assign4.c
@@ -5,11 +5,53 @@
     searched, or displayed.
 */
 #include <stdio.h>
-#inlcude <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 
+//prototypes
+void loadCourseInfo();
+void addCourseInfo();
+void searchCourseInfo();
+void displayCourseInfo();
+
+typedef struct {
+    int courseID;
+    char courseName[50];
+    char courseCode[12];
+    char term[6];
+} CourseInfo;
 
 
 int main(void){
+    
+    
+    
+}
 
+/*
+
+*/
+void loadCourseInfo(){
+    
+}
+
+/*
+
+*/
+void addCourseInfo(){
+    
+}
+
+/*
+
+*/
+void searchCourseInfo(){
+    
+}
+
+/*
+
+*/
+void displayCourseInfo(){
+    
 }
