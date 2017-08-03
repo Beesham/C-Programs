966add1e2c6b720438b9bc1767c4cec699d260ef
Beesham Sarendranauth <sarendranauth.beesham@gmail.com>
2017-08-03 19:02:57 -0400

    Implement terminate func in assign5
    In display func, the data is played before progression of pointer
    In load func, space should be allocated for each student not just once

--- Dynamic Linked Lists and File Manipulation/assign5.c
+++ Dynamic Linked Lists and File Manipulation/assign5.c
@@ -18,13 +18,13 @@
 #define EOF_MARKER "***"
 
 //Course info struct
-typedef struct {
+typedef struct courseInfo {
     int courseId;
     char courseName[COURSE_NAME_LEN];
 } CourseInfo;
 
 //Student info struct
-typedef struct {
+typedef struct studentInfo {
     char studentId[STUDENT_ID_LEN + 1];
     char firstName[FIRST_NAME_LEN + 1];
     char lastName[LAST_NAME_LEN + 1];
@@ -54,6 +54,7 @@ int main(void){
     
     loadStudentInfo(fileName, &studentInfoPtr);
     displayStudentInfo(&studentInfoPtr);
+    terminate(&studentInfoPtr);
 }
 
 /*
@@ -89,12 +90,13 @@ void searchStudentName(){
 */
 void displayStudentInfo (StudentInfoPtr *head) {
     StudentInfoPtr currentStudentInfoPtr = *head;
-
+    
+    if(currentStudentInfoPtr == NULL) puts("List is empty");
+    
     while (currentStudentInfoPtr != NULL) {
-        currentStudentInfoPtr = (StudentInfoPtr) currentStudentInfoPtr->next;
         printf("%s", currentStudentInfoPtr->firstName);    
+        currentStudentInfoPtr = (StudentInfoPtr) currentStudentInfoPtr->next;
     }
-
 }
 
 /*
@@ -110,19 +112,21 @@ void saveStudentInfo(){
 void loadStudentInfo (const char *fileName, StudentInfoPtr *startStudentInfoPtr) {
     FILE *filePtr;
 
-    StudentInfoPtr newStudentInfoPtr = NULL;    
     StudentInfoPtr previousStudentInfoPtr = NULL;
     StudentInfoPtr currentStudentInfoPtr = *startStudentInfoPtr;
         
-    //If no space is available return
-    if ((newStudentInfoPtr = malloc(sizeof(StudentInfo))) == NULL) {
-        puts("Unable to allocate memory");
-        return;
-    }
+    
     
     if ((filePtr = fopen(fileName, "r")) == NULL) puts("FILE COULD NOT BE OPENED");
     else{
         while (1) {
+            StudentInfoPtr newStudentInfoPtr = NULL;    
+
+            //If no space is available return
+            if ((newStudentInfoPtr = malloc(sizeof(StudentInfo))) == NULL) {
+                puts("Unable to allocate memory");
+                break;
+            }
             
             //Reads student info
             {
@@ -142,13 +146,15 @@ void loadStudentInfo (const char *fileName, StudentInfoPtr *startStudentInfoPtr)
             //Reads each course data
             for (int i = 0; i < newStudentInfoPtr->numOfAttentingCourses; i++) {
                 fscanf(filePtr, "%29s %d", newStudentInfoPtr->courseInfoArr[i].courseName, &(newStudentInfoPtr->courseInfoArr[i]).courseId);
-                printf("%s %d\n", newStudentInfoPtr->courseInfoArr[i].courseName, (newStudentInfoPtr->courseInfoArr[i].courseId));
             }
-            
+
+            printf("%s\n", newStudentInfoPtr->firstName);
+
             newStudentInfoPtr->next = NULL;
             
             //Loops through the list to find suitable node to insert to
-            while (currentStudentInfoPtr != NULL && newStudentInfoPtr->studentId > currentStudentInfoPtr->studentId) {
+            while (currentStudentInfoPtr != NULL && 
+                    strtol(newStudentInfoPtr->studentId, NULL, 10) > strtol(currentStudentInfoPtr->studentId, NULL, 10)) {
                 previousStudentInfoPtr = currentStudentInfoPtr;
                 currentStudentInfoPtr = (StudentInfoPtr) currentStudentInfoPtr->next;
             }  
@@ -161,17 +167,28 @@ void loadStudentInfo (const char *fileName, StudentInfoPtr *startStudentInfoPtr)
                 previousStudentInfoPtr->next = newStudentInfoPtr;
                 newStudentInfoPtr->next = currentStudentInfoPtr;
             }
-
         }
-
     }
 }
 
 /*
-
+    terminate: free the allocated space used in the linked list
+    input: the beginning of the list
 */
-void terminate(){
-    
+void terminate(StudentInfoPtr *head){
+    StudentInfoPtr currentStudentInfoPtr = *head;
+    StudentInfoPtr hold = NULL;
+
+    if(currentStudentInfoPtr == NULL) puts("List is empty");
+    else{
+        while (currentStudentInfoPtr != NULL) {
+            hold = (StudentInfoPtr) currentStudentInfoPtr->next;
+            currentStudentInfoPtr = (StudentInfoPtr) currentStudentInfoPtr->next;
+            free(hold);
+        }
+        
+        *head = NULL;
+    }
 }
 
 
